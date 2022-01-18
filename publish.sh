f0b60161b75a0c8785df8ac65a39352ab01b54da
Jay K <jay94ks@gmail.com>
2022-01-19 00:33:41 +0900

    added scripts that publishes the nuget packages to github repository.

---
+++ publish.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+dotnet pack --configuration Release 
+dotnet nuget push "src/Backrole.Core/bin/Release/Backrole.Core.1.0.0.nupkg" --source "github"
+dotnet nuget push "src/Backrole.Core.Abstractions/bin/Release/Backrole.Core.Abstractions.1.0.0.nupkg" --source "github"
