5bae9014be8a2d9d65c46b8815552477ab5c919a
Jay K <jay94ks@gmail.com>
2022-02-06 07:08:46 +0900

    Nuget Packaging information modified.

---
+++ orp/publish.bat
@@ -0,0 +1,7 @@
+@echo off
+
+dotnet pack --configuration Release 
+dotnet nuget push "src/Backrole.Orp/bin/Release/Backrole.Orp.1.0.0.nupkg" --source "github"
+dotnet nuget push "src/Backrole.Orp.Abstractions/bin/Release/Backrole.Orp.Abstractions.1.0.0.nupkg" --source "github"
+
+pause
